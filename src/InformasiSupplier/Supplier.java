a11ce14e2f5b7f13ad38842daea199ee883fbdc7
denayprm <152400121+denayprm@users.noreply.github.com>
2023-11-30 13:22:45 +0700

    Update Supplier.java

--- src/InformasiSupplier/Supplier.java
+++ src/InformasiSupplier/Supplier.java
@@ -1,7 +1,26 @@
 package InformasiSupplier;
 
 public class Supplier {
-    public static void main(String[] args) {
-        System.out.println("Ada gaks??");
+    private int idSupplier;
+    private String namaSupplier;
+    private String alamatSupplier;
+    private String noTelpSupplier;
+    private String jenisProduk;
+    private String infoPembayaran;
+    public Supplier(int idSupplier, String namaSupplier, String alamatSupplier,
+                    String noTelpSupplier, String jenisProduk, String infoPembayaran) {
+        this.idSupplier = idSupplier;
+        this.namaSupplier = namaSupplier;
+        this.alamatSupplier = alamatSupplier;
+        this.noTelpSupplier = noTelpSupplier;
+        this.jenisProduk = jenisProduk;
+        this.infoPembayaran = infoPembayaran;
     }
+
+    public int getIdSupplier() {return idSupplier;}
+    public String getNamaSupplier() {return namaSupplier;}
+    public String getAlamatSupplier() {return alamatSupplier;}
+    public String getNoTelpSupplier() {return noTelpSupplier;}
+    public String getJenisProduk() {return jenisProduk;}
+    public String getInfoPembayaran() {return infoPembayaran;}
 }
