f830634a25a6a2eb9e893702dd26a97e726c33db
denayprm <152400121+denayprm@users.noreply.github.com>
2023-12-03 15:13:33 +0700

    gui update1

--- src/InformasiSupplier/MainInformasiSupplier.java
+++ src/InformasiSupplier/MainInformasiSupplier.java
@@ -1,8 +1,11 @@
 package InformasiSupplier;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 public class MainInformasiSupplier {
     private JRadioButton rbBahanMakanan;
@@ -10,13 +13,20 @@ public class MainInformasiSupplier {
     private JRadioButton rbElektronik;
     private JRadioButton rbPakaian;
     private JRadioButton rbSemuanya;
+    private JComboBox comboBox1;
+    private JTable table2;
     private JTable table1;
+
 
     public MainInformasiSupplier() {
         rbBahanMakanan.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                if (rbBahanMakanan.isSelected()) {
 
+                } else {
+
+                }
             }
         });
     }
---
+++ src/InformasiSupplier/SupplierData.java
@@ -0,0 +1,12 @@
+package InformasiSupplier;
+
+public class SupplierData {
+    private String id_supplier;
+    private String nama_supplier;
+    private String alamat_supplier;
+    private Double no_telp_supplier;
+    private String jenis_produk;
+    private String info_pembayaran;
+
+}
+
